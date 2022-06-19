d96a959115063f3595836416036509baa7a627eb
Weblite <106863678+weblite-net@users.noreply.github.com>
2022-06-19 23:17:38 +0530

    "modified autocompilation"

--- JS/fullscreenScript.js
+++ JS/fullscreenScript.js
@@ -3,6 +3,11 @@ let styleLink = document.querySelector('link');
 //autocompile state
 let autoState = false;
 
+//Autocompile Config
+let interval;
+
+
+
 
 styleLink.setAttribute('href', `${localStorage.getItem('mode')}Styles.css`);
 
@@ -17,16 +22,22 @@ window.addEventListener('focus', () => {
 
 
 
-document.getElementById('output').innerHTML = localStorage.getItem('markup');
-
 let autoCompileBtn = document.getElementById("autoCompile");
+document.getElementById('output').innerHTML = localStorage.getItem('markup');
+autoCompileBtn.style.background = "#ff3333d1";
+
 autoCompileBtn.addEventListener('click', () => {
     autoState = !autoState;
     if(autoState){
         autoCompileBtn.innerText = "Autocompile On !!! Recompiles by itself";
+        autoCompileBtn.style.background = "#01754b";
+        interval = setInterval(displayOutput, 1500);
 
     }else{
         autoCompileBtn.innerText = "Autocompile turned Off";
+        autoCompileBtn.style.background = "#ff3333d1";
+
+        clearInterval(interval);
         
     }
 
